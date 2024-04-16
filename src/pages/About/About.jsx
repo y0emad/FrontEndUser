ebb0cf3a191085986e9f887d53a1bbcc41309652
yousef.emad <y.emad85@yahoo.com>
2024-04-16 23:37:50 +0200

    About  work

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -4,6 +4,8 @@ import helwanLogo from "../../images/helwanLogo.png";
 import { useEffect } from "react";
 import { useTranslation } from "react-i18next";
 import useLocalStorage from "../../hooks/useLocalStorage";
+import divOne from "../../images/divOne.jpg";
+import divTwo from "../../images/divTwo.png";
 export function About() {
   const [t, i18n] = useTranslation("global");
   const [lang, setLang] = useLocalStorage("lang", "ar");
@@ -15,7 +17,7 @@ export function About() {
     <>
       <div className="grid md:grid-cols-2 grid-cols-1 justify-items-center items-center">
         <div>
-          <div className=" relative w-[500px] h-[300px] mt-[16%]">
+          <div className=" relative  w-[500px] h-[300px] mt-[16%]">
             <div className=" w-[51px] h-[51px] rounded-full bg-[#504000] absolute top-16 ltr:left-[-1px]  rtl:right-[-1px] z-3=20"></div>
             <div className=" w-[103px] h-[103px] rounded-full bg-[#000915] absolute ltr:left-3 rtl:right-[3px] top-3 z-10"></div>
             <div className=" w-[81px] h-[81px] rounded-full bg-gray-200 absolute ltr:left-3  rtl:right-[3px] top-0 z-0"></div>
@@ -45,22 +47,22 @@ export function About() {
           </div>
         </div>
       </div>{" "}
-      <section class="py-14 lg:py-24 relative">
-        <div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8 relative ">
-          <div class="grid grid-cols-1 lg:grid-cols-2 gap-9">
-            <div class="img-box">
+      <section className="py-14 lg:py-24 relative">
+        <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8 relative ">
+          <div className="grid grid-cols-1 lg:grid-cols-2 gap-9">
+            <div className="img-box">
               <img
-                src="https://pagedone.io/asset/uploads/1702034769.png"
-                alt="About Us tailwind page"
-                class="max-lg:mx-auto"
+                src={divOne}
+                alt="About Us"
+                className="max-lg:mx-auto rounded-lg"
               />
             </div>
-            <div class="lg:pl-[100px] flex items-center">
-              <div class="data w-full">
-                <h2 class="font-manrope font-bold text-4xl lg:text-5xl text-gray-200 mb-9 max-lg:text-center relative">
+            <div className="lg:pl-[100px] flex items-center">
+              <div className="data w-full">
+                <h2 className="font-manrope font-bold text-4xl lg:text-5xl text-gray-200 mb-9 max-lg:text-center relative">
                   About Us{" "}
                 </h2>
-                <p class="font-normal text-xl leading-8 text-gray-200 max-lg:text-center max-w-2xl mx-auto">
+                <p className=" GEThreeLight  text-xl  leading-8 text-gray-200 max-lg:text-center max-w-2xl mx-auto">
                   {t("About.divOne")}
                 </p>
               </div>
@@ -68,30 +70,26 @@ export function About() {
           </div>
         </div>
       </section>
-      <section class="py-14 lg:py-24 relative">
-        <div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8 relative ">
-          <div class="grid grid-cols-1 lg:grid-cols-2 lg:gap-9 ">
-            <div class="lg:pr-24 flex items-center">
-              <div class="data w-full">
+      <section className="py-14 lg:py-24 relative">
+        <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8 relative ">
+          <div className="grid grid-cols-1 lg:grid-cols-2 lg:gap-9 ">
+            <div className="lg:pr-24 flex items-center">
+              <div className="data w-full">
                 <img
-                  src="https://pagedone.io/asset/uploads/1702034785.png"
-                  alt="About Us tailwind page"
-                  class="block lg:hidden mb-9 mx-auto"
+                  src={divTwo}
+                  alt="About Us image"
+                  className="block lg:hidden mb-9 mx-auto"
                 />
-                <h2 class="font-manrope font-bold text-4xl lg:text-5xl text-gray-200 mb-9 max-lg:text-center">
+                {/* <h2 className="font-manrope font-bold text-4xl lg:text-5xl text-gray-200 mb-9 max-lg:text-center">
                   We are Creative Since 2005
-                </h2>
-                <p class="font-normal text-xl leading-8 text-gray-200 max-lg:text-center max-w-2xl mx-auto">
+                </h2> */}
+                <p className="GEThreeLight text-xl  leading-8 text-gray-200 max-lg:text-center max-w-2xl mx-auto">
                   {t("About.divTwo")}
                 </p>
               </div>
             </div>
-            <div class="img-box ">
-              <img
-                src="https://pagedone.io/asset/uploads/1702034785.png"
-                alt="About Us tailwind page"
-                class="hidden lg:block "
-              />
+            <div className="img-box ">
+              <img src={divTwo} alt="About Us" className="hidden lg:block " />
             </div>
           </div>
         </div>

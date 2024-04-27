0c5d25a5d11ae60c2ed3865e5885262ab94d7c25
MostafaElkhashab <mostafaelkhashab79@gmail.com>
2024-04-27 05:21:13 +0300

    7pp ya omda

--- src/main.jsx
+++ src/main.jsx
@@ -22,6 +22,7 @@ i18next.init({
 });
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
+    
     <I18nextProvider i18n={i18next}>
       <RouterProvider router={router} />
     </I18nextProvider>

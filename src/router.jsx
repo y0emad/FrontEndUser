e64017a1c7acec0440cb99418ea338286ba0566a
yousef.emad <y.emad85@yahoo.com>
2024-04-27 05:59:40 +0300

    Final 1

--- src/router.jsx
+++ src/router.jsx
@@ -2,7 +2,7 @@ import { createBrowserRouter } from "react-router-dom";
 import { HomeFunc } from "./pages/Home/Home";
 import { LayoutMain } from "./layouts/LayoutMain";
 import { LayoutError } from "./layouts/LayoutError";
-import { LogIn } from "./pages/Log_in/LogIn";
+
 import { MyProjects } from "./pages/My_Projects/MyProjects";
 import { About } from "./pages/About/About";
 import { ProductFunc } from "./pages/Product/Product";
